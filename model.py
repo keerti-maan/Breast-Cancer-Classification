5b1428da64950c85e94c2026cbf9befa2538a694
keerti <keerti.mili@gmail.com>
2020-08-30 10:49:43 -0400

    normalization

--- model.py
+++ model.py
@@ -8,6 +8,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+
 
 #importing dataset
 from sklearn.datasets import load_breast_cancer
@@ -20,3 +24,16 @@ df=pd.DataFrame(np.c_[cancer['data'],cancer['target']], columns= np.append(cance
 sns.countplot(df['target'])
 sns.pairplot(df, hue='target',vars=['mean radius','mean smoothness','mean area','mean perimeter'])
 sns.heatmap(df.corr())
+
+# Model
+X=df.drop(['target'], axis=1)
+y=df['target']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2, random_state=42)
+
+model=SVC()
+
+# normalization and optimization
+min_train=X_train.min()
+range_train=(X_train-min_train).max()
+X_norm= (X_train-min_train)/range_train
+
